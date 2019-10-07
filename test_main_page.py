d2745beef064e7885ece93821af5fd2636ee3b9a
pegas511 <pegas511@mail.ru>
2019-10-07 16:51:05 +0300

    Введены проверки с класса LoginPage в test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,6 @@
 from .pages.main_page import MainPage
-
-
+from .pages.login_page import LoginPage
+#link = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -9,6 +9,9 @@ def test_guest_can_go_to_login_page(browser):
     page.open()                    # открываем страницу
     page.go_to_login_page()        # выполняем метод страницы - 
     #переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url) #инициализация перехода на другую страницу
+    login_page.should_be_login_page()
+    
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
