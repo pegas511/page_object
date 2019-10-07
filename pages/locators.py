440cf16cfc7fab27714faa5dbc5f444b46f43f78
pegas511 <pegas511@mail.ru>
2019-10-07 15:41:43 +0300

    Файл создан для хранения всех локаторов

---
+++ pages/locators.py
@@ -0,0 +1,15 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+
+class LoginPageLocators():
+    LOGIN_EMAIL = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
+
+    REG_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REG_PASSWORD_1 = (By.CSS_SELECTOR, "#id_registration-password1")
+    REG_PASSWORD_2 = (By.CSS_SELECTOR, "#id_registration-password2")
