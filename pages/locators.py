06a05c5d37dfa3c42d264d94e9551bdc9b8584af
pegas511 <pegas511@mail.ru>
2019-10-08 14:45:16 +0300

    Введен класс локаторов ProductPageLocators для ProductPage

--- pages/locators.py
+++ pages/locators.py
@@ -13,3 +13,10 @@ class LoginPageLocators():
     REG_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
     REG_PASSWORD_1 = (By.CSS_SELECTOR, "#id_registration-password1")
     REG_PASSWORD_2 = (By.CSS_SELECTOR, "#id_registration-password2")
+
+class ProductPageLocators():
+    BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    MESSAGE_ABOUT_ADDING = (By.CSS_SELECTOR, "div.alertinner strong")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
+    MESSAGE_BASKET_TOTAL = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
