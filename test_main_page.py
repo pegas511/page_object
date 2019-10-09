c75ba63cc96c5648d7c36ec1b7df5e6c8823a820
pegas511 <pegas511@mail.ru>
2019-10-09 20:03:50 +0300

    Изменен конструктор MainPage на BasePage

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,10 @@
-from .pages.main_page import MainPage
+from .pages.main_page import BasePage
 from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link) # инициализируем Page Object, 
+    page = BasePage(browser, link) # инициализируем Page Object, 
     #передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                    # открываем страницу
     page.go_to_login_page()        # выполняем метод страницы - 
@@ -15,6 +15,7 @@ def test_guest_can_go_to_login_page(browser):
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    page = BasePage(browser, link)
     page.open()
     page.should_be_login_link()
+
