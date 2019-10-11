b73002d0cbe194e8d26f04b84847a3ebcf5e7bdf
pegas511 <pegas511@mail.ru>
2019-10-11 09:46:07 +0300

    Добавлен метод test_guest_cant_see_product_in_basket_opened_from_product_page, проверяет, что корзина пуста и сообщение корзина пуста

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 import pytest
 from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -58,3 +59,11 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    page.should_be_no_goods_in_basket()
+    page.should_be_text_in_basket_that_basket_is_empty()
