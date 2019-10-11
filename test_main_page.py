83219a34f7a620d786b2cfbe3faab542c8591d8d
pegas511 <pegas511@mail.ru>
2019-10-11 16:38:26 +0300

    Добавлен объединяющий класс class TestLoginFromMainPage для методов test_guest_can_go_to_login_page, test_guest_should_see_login_link

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,7 @@
 from .pages.main_page import BasePage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
+import pytest
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -32,3 +33,22 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     page.should_be_no_goods_in_basket()
     page.should_be_text_in_basket_that_basket_is_empty()
 
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    # не забываем передать первым аргументом self 
+                           
+    def test_guest_can_go_to_login_page(self, browser):     
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = BasePage(browser, link) # инициализируем Page Object, 
+        #передаем в конструктор экземпляр драйвера и url адрес 
+        page.open()                    # открываем страницу
+        page.go_to_login_page()        # выполняем метод страницы - 
+        #переходим на страницу логина
+        login_page = LoginPage(browser, browser.current_url) #инициализация перехода на другую
+        login_page.should_be_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = BasePage(browser, link)
+        page.open()
+        page.should_be_login_link()
