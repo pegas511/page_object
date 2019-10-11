fb1320f6a5e5e0acec47eac3aa2f3e458ea080e3
pegas511 <pegas511@mail.ru>
2019-10-11 09:47:57 +0300

    Добавлен тест test_guest_cant_see_product_in_basket_opened_from_main_page, проверяющий, что в корзине нет товаров и сообщение о том, что корзина пуста

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 from .pages.main_page import BasePage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -18,4 +19,16 @@ def test_guest_should_see_login_link(browser):
     page = BasePage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    """1 Гость открывает главную страницу 
+       2 Переходит в корзину по кнопке в шапке сайта
+       3 Ожидаем, что в корзине нет товаров
+       4 Ожидаем, что есть текст о том что корзина пуста"""
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    page.should_be_no_goods_in_basket()
+    page.should_be_text_in_basket_that_basket_is_empty()
 
