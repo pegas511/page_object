86adc43336eb26f13a56f95e61abc51a6bb7610f
pegas511 <pegas511@mail.ru>
2019-10-11 16:36:52 +0300

    Добавлен класс TestUserAddToBasketFromProductPage, тестовый класс, который содержит методы для зарегистрированных пользователей

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,8 @@
 import pytest
+import time
 from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -67,3 +69,33 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.go_to_basket()
     page.should_be_no_goods_in_basket()
     page.should_be_text_in_basket_that_basket_is_empty()
+
+@pytest.mark.user
+class TestUserAddToBasketFromProductPage():
+    #добавить фикструку сетап с регистрацией
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        self.email = str(time.time()) + "@fakemail.org"
+        self.password = "9595959595"
+        link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+        page = LoginPage(browser, link)
+        page.open()
+        page.register_new_user(self.email, self.password)
+        page.should_be_authorized_user()
+        time.sleep(5)
+        
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.find_button_basket()
+        #page.solve_quiz_and_get_code()
+        page.should_be_message_about_adding()
+        page.should_be_message_basket_total()
