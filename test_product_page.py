88a55657ad1362eca32c1e5e42fe12742b03e449
pegas511 <pegas511@mail.ru>
2019-10-14 14:53:35 +0300

    Убраны . в импортах, чтобы запускалось без ошибок

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,8 @@
 import pytest
 import time
-from .pages.product_page import ProductPage
-from .pages.basket_page import BasketPage
-from .pages.login_page import LoginPage
+from pages.product_page import ProductPage
+from pages.basket_page import BasketPage
+from pages.login_page import LoginPage
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
